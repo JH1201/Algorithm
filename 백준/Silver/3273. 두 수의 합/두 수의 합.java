003deba3d95feee43e46fdeb011d30bde964c3df
Jihoon Kim <96639809+JH1201@users.noreply.github.com>
2025-10-23 21:59:41 +0900

    [Silver III] Title: 두 수의 합, Time: 816 ms, Memory: 105244 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -4,41 +4,35 @@ import java.util.*;
 public class Main {
     public static void main(String args[]) throws IOException 
     {
+        Scanner sc = new Scanner(System.in);
+        int N =sc.nextInt();
 
-    Scanner sc = new Scanner(System.in);
-    
-    int N = sc.nextInt();
+        ArrayList<Integer> list = new ArrayList<>();
 
-    ArrayList<Integer> list = new ArrayList<>();
-    for(int i=0; i<N; i++) {
-        int tmp = sc.nextInt();
-        list.add(tmp);
-    }
+        for(int i=0; i<N; i++) {
+            int tmp = sc.nextInt();
 
-    int K = sc.nextInt();
-
-    Collections.sort(list);
-
-    int left = 0;
-    int right = list.size()-1;
-
-    int res = 0;
-    while(left < right) {
-        int sum = list.get(left) + list.get(right);
-
-        if(sum == K) {
-            res++;
-            left++;
+            list.add(tmp);
         }
-        else if(sum < K) {
-            left++;
-        }
-        else {
-            right--;
-        }
-    }
 
-    System.out.println(res);
-    }
+        int X = sc.nextInt();
 
+        Collections.sort(list);
+
+        int left = 0;
+        int right = list.size()-1;
+
+        int res = 0;
+        while(left < right) {
+            int sum = list.get(left) + list.get(right);
+            if(sum == X) {
+                res += 1;
+                left++;
+            }
+            else if(sum < X) left++;
+            else right--;
+        }
+
+        System.out.println(res);
+    }
 }
