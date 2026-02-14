428038e1186f1a285347ff43fbb69622a5b01a51
Jihoon Kim <96639809+JH1201@users.noreply.github.com>
2026-02-14 15:26:26 +0900

    [level 3] Title: 입국심사, Time: 72.47 ms, Memory: 85.3 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/README.md
+++ 프로그래머스/3/43238. 입국심사/README.md
@@ -1,10 +1,10 @@
 # [level 3] 입국심사 - 43238 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238) 
 
 ### 성능 요약
 
-메모리: 97.6 MB, 시간: 64.29 ms
+메모리: 85.3 MB, 시간: 72.47 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 12일 22:50:44
+2026년 02월 14일 15:26:23
 
 ### 문제 설명
 
--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -2,30 +2,47 @@ import java.util.*;
 
 class Solution {
     public  long solution(int n, int[] times) {
-        int max = Arrays.stream(times).max().getAsInt();
-
-        long start = 1;
-        long end = (long)max * n;
-        long answer = end;
-
-        while(start <= end) {
-            long mid = (start + end) / 2;
-            long num =0; // 심사관이 처리하는 사람수
-
-            // 심사관이 처리하는 사람수
-            for(int i=0; i<times.length; i++) {
-                num += mid/times[i];
-            }
-
-            if(num < n) {
-                start = mid + 1;
-
-            } else {
-                answer = mid;
-                end = mid - 1;
-            }
+        long max = 0;
+        
+        for(int i=0; i<times.length; i++) {
+            if(max < times[i]) max = times[i];
         }
-
+        
+        System.out.println("[max = " + max + "]");
+        
+        long start = 1;
+        long end = (long)n * max;
+        long answer = end;
+        
+        while(start <= end) {
+            System.out.println("start = " + start);
+            System.out.println("end = " + end);
+            
+            long mid = (start+end)/2;
+            System.out.println("mid = " + mid);
+            
+            long num = 0;
+            
+            for(int j=0; j<times.length; j++) {
+                num += mid/times[j];
+            }
+            System.out.println("num = " + num);
+            
+            if(num < n) {
+                start = mid+1;
+                
+            }
+            else {
+                answer = mid;
+                end = mid-1;
+            }
+            
+            System.out.println("answer = " + answer);
+            
+            System.out.println("------------------------- ");
+            
+        }
+        
 
         return answer;
     }
