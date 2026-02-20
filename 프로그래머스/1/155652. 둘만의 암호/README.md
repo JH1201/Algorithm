b923f2f88c9dc13f66daf2342e3fea78a3e75480
Jihoon Kim <96639809+JH1201@users.noreply.github.com>
2026-02-20 14:31:16 +0900

    [level 1] Title: 둘만의 암호, Time: 0.11 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/README.md
+++ 프로그래머스/1/155652. 둘만의 암호/README.md
@@ -1,10 +1,10 @@
 # [level 1] 둘만의 암호 - 155652 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155652?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155652) 
 
 ### 성능 요약
 
-메모리: 82.3 MB, 시간: 0.13 ms
+메모리: 78.8 MB, 시간: 0.11 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 19일 22:49:36
+2026년 02월 20일 14:31:13
 
 ### 문제 설명
 
--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -3,26 +3,30 @@ class Solution {
         String answer = "";
         
         boolean[] isSkip = new boolean[26];
-        for(char c :skip.toCharArray()) {
-            isSkip[c-'a'] = true;
-        }
         
         StringBuilder sb = new StringBuilder();
         
-        for (char c : s.toCharArray()) {
-            int moved = 0;
-
-            while (moved < index) {
+        for(char c : skip.toCharArray()) {
+            isSkip[c-'a'] = true;
+        }
+        
+        for(char c : s.toCharArray()) {
+            int move = 0;
+            
+            while(move < index) {
                 c++;
+                
                 if(c > 'z') c = 'a';
                 
-                if(isSkip[c - 'a']) continue;
-                moved++; 
+                if(isSkip[c-'a']) continue;
+                move++;
             }
-
             sb.append(c);
         }
         
-        return sb.toString();
+        answer = sb.toString();
+        
+        return answer;
+        
     }
 }
