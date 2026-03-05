f6639129c4ae0fc237c203d4510bef1182d488c2
Jihoon Kim <96639809+JH1201@users.noreply.github.com>
2026-03-05 14:07:11 +0900

    [level 3] Title: 입국심사, Time: 52.82 ms, Memory: 90.5 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -2,47 +2,36 @@ import java.util.*;
 
 class Solution {
     public  long solution(int n, int[] times) {
+        
         long max = 0;
+        long answer = 0;
         
         for(int i=0; i<times.length; i++) {
             if(max < times[i]) max = times[i];
         }
         
-        System.out.println("[max = " + max + "]");
-        
         long start = 1;
         long end = (long)n * max;
-        long answer = end;
         
         while(start <= end) {
-            System.out.println("start = " + start);
-            System.out.println("end = " + end);
             
-            long mid = (start+end)/2;
-            System.out.println("mid = " + mid);
-            
+            long mid = (start + end) / 2;
             long num = 0;
             
-            for(int j=0; j<times.length; j++) {
-                num += mid/times[j];
+            // 걸리는 시간동안 처리할 수 있는 인원 수
+            for(int i=0; i<times.length; i++) {
+                num += mid/times[i];
             }
-            System.out.println("num = " + num);
             
             if(num < n) {
-                start = mid+1;
-                
+                start = mid + 1;
             }
             else {
+                end = mid - 1;
                 answer = mid;
-                end = mid-1;
             }
             
-            System.out.println("answer = " + answer);
-            
-            System.out.println("------------------------- ");
-            
         }
-        
 
         return answer;
     }
