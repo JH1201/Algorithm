ef1ae7d7f3e99d5783e0bb42dd559507dfaa3832
Jihoon Kim <96639809+JH1201@users.noreply.github.com>
2026-03-06 14:05:13 +0900

    [level 2] Title: 구명보트, Time: 10.78 ms, Memory: 57.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        
+        Arrays.sort(people);
+        
+        int start = 0;
+        int end = people.length-1;
+        while(start <= end) {
+            
+            if(people[end--] + people[start] <= limit) start++;
+            answer++;
+            
+        }
+        
+       
+        return answer;
+    }
+}
