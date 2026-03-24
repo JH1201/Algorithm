30b07efaf6a744d296d5ea6e24a052bbe183d64b
Jihoon Kim <96639809+JH1201@users.noreply.github.com>
2026-03-24 19:23:32 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 71.13 ms, Memory: 97.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -3,32 +3,30 @@ import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
-        
-        HashMap<String, Integer> map = new HashMap<>();
-        
-        for(int i=0; i<participant.length; i++) {
-            if(map.get(participant[i]) != null) {
-                if(map.get(participant[i]) >= 1) {
-                    int tmp = map.get(participant[i]) + 1;
-                    map.put(participant[i], tmp);
-                    continue;
-                }
-            }
             
-            map.put(participant[i], 1);
-        }
+        HashMap<String, Integer> parti = new HashMap<>();
         
-        for(int j=0; j<completion.length; j++) {
-            if(map.get(completion[j]) != null) {
-                map.replace(completion[j], map.get(completion[j])-1);
+        int cnt = 1;
+        for(String p : participant) {
+            if(parti.get(p) == null) {
+                parti.put(p, cnt);
+            }
+            else {
+                int t = parti.get(p) + 1;
+                parti.put(p, t);
             }
         }
         
-        for(int i=0; i<participant.length; i++) {
-            if(map.get(participant[i]) != 0 && map.get(participant[i]) > 0) {
-                answer = participant[i];
+        for(String c : completion) {
+            int t = parti.get(c)-1;
+            parti.put(c, t);
+        }
+        
+        for(String p : participant) {
+            if(parti.get(p) > 0){
+                answer = p;
                 break;
-            }
+            }   
         }
          
         return answer;
