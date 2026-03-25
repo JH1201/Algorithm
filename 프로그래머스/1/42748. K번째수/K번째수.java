93b15e04941ab5249604298993dee2adab06ad4a
Jihoon Kim <96639809+JH1201@users.noreply.github.com>
2026-03-25 22:01:17 +0900

    [level 1] Title: K번째수, Time: 0.07 ms, Memory: 85.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        
+        //Arrays.sort(array);
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int a=0; a<commands.length; a++) {
+            int i=commands[a][0];
+            int j=commands[a][1];
+            int k=commands[a][2];
+            
+            for(int b=i-1; b<j; b++) {
+                list.add(array[b]);
+            }
+            
+            Collections.sort(list);
+            answer[a] = list.get(k-1);
+            
+            list.clear();
+        }
+        
+        return answer;
+    }
+}
