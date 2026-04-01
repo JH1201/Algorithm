9b189ec299c3b4383a2fb714289fe7e7947f25b0
Jihoon Kim <96639809+JH1201@users.noreply.github.com>
2026-04-02 08:38:20 +0900

    [Bronze II] Title: Hashing, Time: 172 ms, Memory: 17788 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+	    int num = sc.nextInt();
+	    sc.nextLine();
+	    String str = sc.nextLine();
+	    
+	    HashMap<Character, Integer> map = new HashMap<>();
+	    
+	    for(int i=0; i<26; i++) {
+	        char c = (char)('a' + i);
+	        
+	        map.put(c, i+1);
+	        
+	    }
+	    
+	    int sum = 0;
+	    for(int j=0; j<str.length(); j++) {
+	        char c = str.charAt(j);
+	        
+	        sum = sum + (int)(map.get(c) * Math.pow(31, j));
+	    }
+	     
+	    System.out.println(sum);
+		
+		
+	}
+}
