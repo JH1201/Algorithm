7b1a8964274e821e8fb48c9a89eeff1edf3ff71d
Jihoon Kim <96639809+JH1201@users.noreply.github.com>
2026-04-02 21:39:27 +0900

    [level 2] Title: 조이스틱, Time: 10.11 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/README.md
+++ 프로그래머스/2/42860. 조이스틱/README.md
@@ -1,10 +1,10 @@
 # [level 2] 조이스틱 - 42860 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860) 
 
 ### 성능 요약
 
-메모리: 72.8 MB, 시간: 0.08 ms
+메모리: 72.5 MB, 시간: 10.11 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 15일 12:18:43
+2026년 04월 02일 21:39:25
 
 ### 문제 설명
 
--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -2,38 +2,49 @@ class Solution {
     public int solution(String name) {
         int answer = 0;
         
-        String tmp = "";
-        int start = 0;
-        int end = name.length();
-        
-        // 세로
         for(int i=0; i<name.length(); i++) {
-            char c = name.charAt(i);
+            char target = name.charAt(i);
             
-            int up = c - 'A';
-            int down = 'Z' - c + 1;
-            answer += Math.min(up, down);
+            int move = getMoveCont(target);
+            //System.out.println(move);
+            
+            answer += move;
         }
         
-        // 가로
         int len = name.length();
-        int move = len - 1; // 기본: 오른쪽으로만 쭉
-
-        for (int i = 0; i < len; i++) {
-            int next = i + 1;
-
-            // i 다음부터 연속된 'A' 구간 스킵
-            while (next < len && name.charAt(next) == 'A') {
-                next++;
+        int m = len - 1;  // 모든 문자가 A가 아니일 때
+        
+        for(int i=0; i<len; i++) {
+            int next = i+1;
+            
+            while(next < len && name.charAt(next) == 'A') {
+                next += 1;
             }
-
-            // 케이스 1: 오른쪽 갔다가 되돌아오기
-            move = Math.min(move, 2 * i + (len - next));
-
-            // 케이스 2: 뒤쪽부터 먼저 갔다가 되돌아오기
-            move = Math.min(move, i + 2 * (len - next));
+            
+            // 오른쪽으로 갔다가 다시 i 위치로
+            m = Math.min(m, i*2+(len-next));
+                
+            // 왼쪽으로 갔다가 다시 i 위치로
+            m = Math.min(m, i + (len-next)*2);
         }
         
-        return answer+move;
+        
+       
+        return answer+m;
+    }
+    
+    public int getMoveCont(char target) {
+        int move = 0;
+        
+        System.out.println("target = " + (target-'A'));
+        
+        if(target-'A' > 13) {
+            move = 'Z'- target+1;
+        }
+        else {
+            move = target-'A';
+        }
+        
+        return move;
     }
 }
