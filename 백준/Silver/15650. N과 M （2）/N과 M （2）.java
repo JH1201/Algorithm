eff4f0afcd9c28f6b028536fa5cbc211b21904c0
Jihoon Kim <96639809+JH1201@users.noreply.github.com>
2026-04-03 22:15:15 +0900

    [Silver III] Title: N과 M (2), Time: 212 ms, Memory: 18648 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,47 @@
+
+import java.util.*;
+
+
+public class Main
+{
+    static boolean[] visited;
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String arr = sc.nextLine();
+        String[] num = arr.split(" ");
+        int N = Integer.valueOf(num[0]);
+        int K = Integer.valueOf(num[1]);
+        
+        visited = new boolean[N+1];
+        
+        
+        for(int i=1; i<=N; i++) {
+            visited[i] = true;
+            dfs(N, K, i, 1, i + " ");
+            visited[i] = false;
+        }
+        
+    }
+    
+    public static void dfs(int N, int K, int n, int depth, String res) {
+        
+        if(depth == K) {
+            System.out.println(res);
+            return;
+        }
+        
+        for(int i=n+1; i<=N; i++) {
+            if(visited[i]) continue;
+            
+            visited[i] = true;
+            dfs(N, K, i, depth+1, res + i + " ");
+            visited[i] = false;
+        }
+        
+        
+        
+    }
+    
+}
