100ba4d6797b1efae8f4020bb5ce76a65bd2d931
Jihoon Kim <96639809+JH1201@users.noreply.github.com>
2026-04-05 11:36:12 +0900

    [level 2] Title: 구명보트, Time: 9.74 ms, Memory: 57.1 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -9,10 +9,27 @@ class Solution {
         int start = 0;
         int end = people.length-1;
         
-        while(start <= end) {
-            int value = people[start] + people[end--];
-            if(value <= limit) start++;
-            answer++;
+        int sum = 0;
+        
+        while(start<=end) {
+            
+            sum = people[start]+people[end];
+            
+            if(start==end) {
+                answer++;
+                break;
+            }
+            
+            if(sum > limit) {
+                answer++;
+                end--;
+            }
+            else {
+                answer++;
+                end--;
+                start++;
+            }
+            
         }
        
         return answer;
