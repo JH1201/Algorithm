c1f9ab7a0502771ec063dd570e4aaa0d45380835
Jihoon Kim <96639809+JH1201@users.noreply.github.com>
2026-04-15 11:12:16 +0900

    [Silver III] Title: 피보나치 함수, Time: 100 ms, Memory: 14036 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+public class Main
+{
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        int T = Integer.parseInt(br.readLine());
+        
+        // 문제에서 N은 최대 40
+        int[][] dp = new int[41][2];
+
+        // 초기값
+        dp[0][0] = 1; // fibonacci(0) 호출 시 0 출력 1번
+        dp[0][1] = 0; // fibonacci(0) 호출 시 1 출력 0번
+        dp[1][0] = 0; // fibonacci(1) 호출 시 0 출력 0번
+        dp[1][1] = 1; // fibonacci(1) 호출 시 1 출력 1번
+        
+        // 미리 40까지 계산
+        for (int i = 2; i <= 40; i++) {
+            dp[i][0] = dp[i - 1][0] + dp[i - 2][0];
+            dp[i][1] = dp[i - 1][1] + dp[i - 2][1];
+        }
+        
+        for(int i=0; i<T; i++) {
+            int N = Integer.parseInt(br.readLine());
+            sb.append(dp[N][0]).append(" ").append(dp[N][1]).append("\n");
+        }
+        
+        System.out.print(sb.toString());
+    }
+    
+    
+}
