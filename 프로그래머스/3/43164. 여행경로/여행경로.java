181be3456c4c59bef6107e44c7aeb05938ffdad4
Jihoon Kim <96639809+JH1201@users.noreply.github.com>
2026-07-02 22:18:16 +0900

    [level 3] Title: 여행경로, Time: 0.67 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -8,45 +8,46 @@ class Solution {
     
     public String[] solution(String[][] tickets) {
         
-        Arrays.sort(tickets, (a, b) -> {
-            if(a[0].equals(b[0])) {
-                return a[1].compareTo(b[1]);
-            }
+        visited = new boolean[tickets.length];
+        
+        Arrays.sort(tickets, (a,b) ->{
+            if(a[0].equals(b[0])){
+                return a[1].compareTo(b[1]); 
+            }        
+            
             return a[0].compareTo(b[0]);
         });
         
         answer.add("ICN");
-        visited = new boolean[tickets.length];
-        dfs("ICN",tickets, 0);
-        
+        dfs("ICN", tickets, 0);
+       
         return answer.toArray(new String[0]);
     }
     
-    public void dfs(String first, String[][] tickets, int count) {
-        
-        if(found) return;
+    public void dfs(String cur, String[][] tickets, int count) {
         
         if(count == tickets.length) {
             found = true;
-            return ;
+            return;
         }
         
         for(int i=0; i<tickets.length; i++) {
+            if(visited[i]) continue;
+            if(!tickets[i][0].equals(cur)) continue;
             
-            if(visited[i]) {
-                continue;
-            }
+            visited[i] = true;
             
-            if(tickets[i][0].equals(first)) {
-                visited[i] = true;
-                answer.add(tickets[i][1]);
-                dfs(tickets[i][1], tickets, count+1);
-                
-                if(found) return;
-                
-                visited[i] = false;
-                answer.remove(answer.size()-1);
-            }
+            answer.add(tickets[i][1]);
+            
+            dfs(tickets[i][1], tickets, count+1);
+            
+            if(found) continue;
+            // 백트레킹
+            visited[i] = false;
+            answer.remove(answer.size()-1);
         }
+        
+        
     }
+ 
 }
