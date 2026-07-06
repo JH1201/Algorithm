bd3c449f3169e98a26319612fe9e154deef0ff0d
Jihoon Kim <96639809+JH1201@users.noreply.github.com>
2026-07-06 16:13:49 +0900

    [level 2] Title: 2 x n 타일링, Time: 1.06 ms, Memory: 56.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    
+    int answer = 0;
+    
+    public int solution(int n) {
+        
+        int a = 2;
+        int b = 1;
+        
+        if(n <= 3) {
+            return n;
+        }
+        
+        for (int i = 3; i <= n; i++) {
+            answer = (a + b) % 1000000007;
+
+            b = a;
+            a = answer;
+        }
+       
+        return answer;
+    }
+
+ 
+}
