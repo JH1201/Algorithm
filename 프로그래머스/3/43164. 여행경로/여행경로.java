2a112055581255ff19eccb7a9cc691524627d7da
Jihoon Kim <96639809+JH1201@users.noreply.github.com>
2026-07-23 15:48:26 +0900

    [level 3] Title: 여행경로, Time: 0.59 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -2,52 +2,53 @@ import java.util.*;
 
 class Solution {
     
+    Stack<String> s = new Stack<>();
     ArrayList<String> answer = new ArrayList<>();
     boolean[] visited;
-    boolean found = false;
+    boolean flag = false;
     
     public String[] solution(String[][] tickets) {
         
         visited = new boolean[tickets.length];
         
-        Arrays.sort(tickets, (a,b) ->{
-            if(a[0].equals(b[0])){
-                return a[1].compareTo(b[1]); 
-            }        
-            
+        // tickets 정렬
+        Arrays.sort(tickets, (a,b) -> {
+            if(a[0].equals(b[0])) {
+                return a[1].compareTo(b[1]);
+            }
             return a[0].compareTo(b[0]);
-        });
+        });        
         
         answer.add("ICN");
         dfs("ICN", tickets, 0);
-       
+      
         return answer.toArray(new String[0]);
     }
     
-    public void dfs(String cur, String[][] tickets, int count) {
+    public void dfs(String curC, String[][] tickets, int cnt)  {
         
-        if(count == tickets.length) {
-            found = true;
-            return;
+        // 탈출 조건
+        if(cnt == tickets.length) {
+            flag = true;
+            return ;
         }
         
         for(int i=0; i<tickets.length; i++) {
+            
             if(visited[i]) continue;
-            if(!tickets[i][0].equals(cur)) continue;
+            if(!curC.equals(tickets[i][0])) continue;
             
+            // 방문 처리
             visited[i] = true;
+            String nextCountry = tickets[i][1];
             
-            answer.add(tickets[i][1]);
+            answer.add(nextCountry);
             
-            dfs(tickets[i][1], tickets, count+1);
-            
-            if(found) continue;
-            // 백트레킹
+            dfs(nextCountry, tickets, cnt+1);
+            if(flag) continue;
             visited[i] = false;
             answer.remove(answer.size()-1);
         }
-        
-        
     }
- 
+    
 }
