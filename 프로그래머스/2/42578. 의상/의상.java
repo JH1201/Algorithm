62bc51dfede500e9f0a2c614e301c2477e5c996d
Jihoon Kim <96639809+JH1201@users.noreply.github.com>
2026-08-18 22:00:33 +0900

    [level 2] Title: 의상, Time: 4.53 ms, Memory: 86.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -4,24 +4,26 @@ class Solution {
     public int solution(String[][] clothes) {
         int answer = 1;
         
-        HashMap<String, ArrayList<String>> map = new HashMap<>();
-        
+        HashMap<String, ArrayList<String>> s = new HashMap<>();
         
         for(int i=0; i<clothes.length; i++) {
-            String category = clothes[i][1];
-            ArrayList<String> list = map.getOrDefault(category, new ArrayList<>(Arrays.asList("0")));
             
+            ArrayList<String> list = s.getOrDefault(clothes[i][1], new ArrayList<>(Arrays.asList("")));
+                
             list.add(clothes[i][0]);
+            s.put(clothes[i][1], list);
             
-            map.put(category, list);
-        }
-        for(Map.Entry<String, ArrayList<String>> entry : map.entrySet()) {
-            ArrayList arr = entry.getValue();
-            System.out.println(entry.getKey() + ", " + arr.size());
-            answer *= arr.size();
         }
         
-        return answer -1;
+        for(Map.Entry<String, ArrayList<String>> entry : s.entrySet()) {
+            ArrayList<String> arr = entry.getValue();
             
+            System.out.println(entry.getKey() + ", " + s.get(entry.getKey()).size());
+            
+            answer *= arr.size();
+        }
+
+        return answer-1;
+
     }
 }
