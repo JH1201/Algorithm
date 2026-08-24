8a2867b865b7be41403458dc948d5ca941f5e4c3
Jihoon Kim <96639809+JH1201@users.noreply.github.com>
2026-08-24 20:24:42 +0900

    [level 3] Title: 순위, Time: 5.24 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -2,36 +2,30 @@ import java.util.*;
 
 class Solution {
     
-    int BIGNUM = 1000000;
+    int  BIGNUM = 1000000;
     
     public int solution(int n, int[][] results) {
+        
         int answer = 0;
         
         int[][] arr = new int[n+1][n+1];
         
-        ArrayList<Integer> list = new ArrayList<>();
-        
-
         // 배열 초기화
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (i == j) {
-                    arr[i][j] = 0;
-                } else {
-                    arr[i][j] = BIGNUM;
-                }
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=n; j++) {
+                if(i == j) arr[i][j] = 0;
+                else arr[i][j] = BIGNUM;
             }
         }
-
-         // 경기 결과 저장
-        for (int i = 0; i < results.length; i++) {
+        
+        for(int i=0; i<results.length; i++) {
             int winner = results[i][0];
             int loser = results[i][1];
-
+            
             arr[winner][loser] = 1;
         }
         
-        // 플로이드-워셜
+        // 플로이드 워샬 알고리즘 적용
         for(int k=1; k<=n; k++) {
             for(int i=1; i<=n; i++) {
                 for(int j=1; j<=n; j++) {
@@ -39,6 +33,7 @@ class Solution {
                 }
             }
         }
+        
         
         for(int i=1; i<=n; i++) {
             int cnt = 0;
@@ -49,6 +44,7 @@ class Solution {
             
             if(cnt == (n-1)) answer++;
         }
+        
         
         return answer;
     }
