d436ec332933fd830870b62b9a0bf4b1bbd2f6f1
Jihoon Kim <96639809+JH1201@users.noreply.github.com>
2026-08-24 20:37:33 +0900

    [level 3] Title: 입국심사, Time: 17.25 ms, Memory: 84.2 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -6,18 +6,12 @@ class Solution {
         long answer = 0;
         long max = 0;
         
-        // 가장 오래 걸리는 시간
         for(int t : times) {
             if(max < t) max = t;
         }
         
-        // 통과하는데 가장 오래 걸리는 시간
-        long end = max * n;
-      
-        // 통과하는데 가장 짧게 걸리는 시간
         long start = 1;
-        
-        System.out.println("end = " + end);
+        long end = n * max;
         
         while(start <= end) {
             
@@ -27,12 +21,11 @@ class Solution {
             for(int t : times) {
                 num += mid/t;
             }
-            
-            if(num < n) start = mid + 1;
-            else {
-                end = mid - 1;
                 
-                // 이후에 더 적은 시간이 걸리는 상황이 있을 수 있음
+            if(num < n) start = mid+1;
+            else {
+                end = mid-1;
+                
                 answer = mid;
             }
         }
