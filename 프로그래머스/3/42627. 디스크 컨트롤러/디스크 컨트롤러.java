82bd6bf14d1445f2b7f1a7c375a1e108e80b31f1
Jihoon Kim <96639809+JH1201@users.noreply.github.com>
2026-08-25 22:09:14 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 3.48 ms, Memory: 85.1 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -4,69 +4,68 @@ class Solution {
     public int solution(int[][] jobs) {
         int answer = 0;
         
-        // 소요시간 짧은 순 > 요청 시각 빠른 순 > 작업의 번호 작은 순
-        PriorityQueue<WorkNode> pQ = new PriorityQueue<>((a, b) -> {
+        PriorityQueue<WorkNode> waitingQueue = new PriorityQueue<>((a, b) -> {
             
-            if(a.duringTime == b.duringTime && a.startTime == b.startTime) {
+            // 작업 시간 > 요청 시각 > 작업 번호 작은 순 
+            if(a.getStartTime() == b.getStartTime()) {
                 return a.workNum - b.workNum;
             }
             
-            else if(a.duringTime == b.duringTime) {
+            return a.getStartTime() - b.getStartTime();
+            
+        });
+        
+        PriorityQueue<WorkNode> resultQ = new PriorityQueue<>((a, b) -> {
+            
+            // 작업 시간 > 요청 시각 > 작업 번호 작은 순 
+            if(a.getDuringTime() == b.getDuringTime()) {
+
+                if(a.getStartTime() == b.getStartTime()) {
+                    return a.workNum - b.workNum;
+                }
+
                 return a.startTime - b.startTime;
             }
             
             return a.duringTime - b.duringTime;
             
-        }); 
-        
-        PriorityQueue<WorkNode> requestQ = new PriorityQueue<>((a, b) -> {
-            
-            if(a.startTime == b.startTime) {
-                return a.workNum - b.workNum;
-            }
-            
-            return a.startTime - b.startTime;
-            
-        }); 
+        });
         
         for(int i=0; i<jobs.length; i++) {
-            int workN = i;            // 작업 번호
-            int startT = jobs[i][0];   // 요청 시간
-            int duringT = jobs[i][1];  // 소요 시간
-                
-            WorkNode wNode = new WorkNode(duringT, startT, workN);
             
-            requestQ.add(wNode);
+            int workNum = i;               // 작업 번호
+            int duringTime = jobs[i][1];   // 작업 시간
+            int startTime = jobs[i][0];   // 요청 시각
+            
+            waitingQueue.add(new WorkNode(duringTime, startTime, workNum));
             
         }
         
-        
         int sum = 0;
-        int tmp = 0;    // 현재 시각
-        while(!pQ.isEmpty() || !requestQ.isEmpty()) {
+        int tmp = 0;
+        while(!waitingQueue.isEmpty() || !resultQ.isEmpty()) {
             
-            // 현재 시간까지 요청이 들어온 작업들을 pQ에 넣음
-            while(!requestQ.isEmpty() && requestQ.peek().getStartTime() <= tmp) {
-                
-                pQ.add(requestQ.poll());
+            
+            
+            while(!waitingQueue.isEmpty() && tmp >= waitingQueue.peek().getStartTime()) {
+                resultQ.add(waitingQueue.poll());
             }
             
-            // 현재 실행할 작업이 없다면
-            // 다음 작업의 요청 시간까지 시간을 이동
-            if(pQ.isEmpty()) {
-                tmp = requestQ.peek().getStartTime();
+            if(resultQ.isEmpty()) {
+                tmp = waitingQueue.peek().getStartTime();
                 continue;
             }
             
+            WorkNode curNode = resultQ.poll();
             
-            WorkNode curNode = pQ.poll();
-            
-            
-            tmp = curNode.getDuringTime() + tmp;
+            tmp = tmp + curNode.getDuringTime();
             sum += tmp - curNode.getStartTime();
+            
         }
-
-        answer = sum/jobs.length;
+        
+        //System.out.println("sum = " + sum + ", tmp = " + tmp);
+        
+        answer = sum / jobs.length;
         
         return answer;
     }
