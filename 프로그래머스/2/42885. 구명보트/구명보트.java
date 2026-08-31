fb4e09989920c9c7d184e6bf3efc1c1f8c3f4c27
Jihoon Kim <96639809+JH1201@users.noreply.github.com>
2026-08-31 10:20:04 +0900

    [level 2] Title: 구명보트, Time: 11.99 ms, Memory: 59.1 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -3,31 +3,27 @@ import java.util.*;
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
-       
-        // 값 정렬
+      
         Arrays.sort(people);
-       
-        int i=0; 
-        int j=people.length-1;
-        int sum = 0;
         
-        while(i<=j) {
+        int start = 0;
+        int end = people.length-1;
+        
+        while(start <= end) {
             
-            sum = people[i] + people[j];
+            int sum = people[start] + people[end];
+            
+            //System.out.print(sum + " ");
             
             if(sum > limit) {
-                j--;
                 answer++;
+                end--;
             }
-            // 보트 최대 인원은 2명
             else {
-                i++;
-                j--;
+                start++;
+                end--;
                 answer++;
             }
-            
-            
-            
         }
         
         return answer;
