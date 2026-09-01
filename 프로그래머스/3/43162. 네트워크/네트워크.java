2622cc14a89fcbf052dfad51c1c7072a2c0ad9b1
Jihoon Kim <96639809+JH1201@users.noreply.github.com>
2026-09-01 14:57:32 +0900

    [level 3] Title: 네트워크, Time: 0.29 ms, Memory: 85.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -3,31 +3,40 @@ import java.util.*;
 class Solution {
     
     static boolean[] visited;
+    int answer = 0;
     
     public int solution(int n, int[][] computers) {
-        int answer = 0;
         
-        visited = new boolean[n+1];
         
-        int len = computers.length;
+        visited = new boolean[computers.length];
         
-        for(int i=1; i<=len; i++) {
+        for(int i=0; i<computers.length; i++) {
             if(!visited[i]) answer++;
             dfs(computers, i);
         }
 
+        //if(answer > computers.length) answer -= 1;
+        
         return answer;
     }
 
     public void dfs(int[][] computers, int idx) {
-        
         if(visited[idx]) return;
+        
+        // 방문 처리
         visited[idx] = true;
         
-        for(int i=1; i<=computers.length; i++) {
+        int cnt = 0;
+        for(int i=0; i<computers.length; i++) {
             if(i == idx) continue;
-            if(computers[idx-1][i-1] == 1) dfs(computers, i);
+            
+            if(computers[i][idx] == 1) {
+                dfs(computers, i);
+                //cnt++;
+            }
         }
+        
+        //if(cnt == 0) answer++;
         
     }
     
