0f028d02a683c1f3583ecfdb70b956951f4b89c1
Jihoon Kim <96639809+JH1201@users.noreply.github.com>
2026-09-03 08:45:55 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.08 ms, Memory: 79.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int n) {
+        
+        long answer = 0;
+        int[] arr = new int[2001];
+        
+        arr[0] = 0;
+        arr[1] = 1;
+        arr[2] = 2;
+        
+        for(int i=3; i<=2000; i++) {
+            arr[i] = (arr[i-1] + arr[i-2]) % 1234567;
+        }
+        
+        answer = arr[n];
+        
+        return answer;
+    }
+    
+}
