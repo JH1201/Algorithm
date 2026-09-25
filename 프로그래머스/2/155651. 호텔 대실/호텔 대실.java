6ebb3fabb4094adab6562571d904856b505e534c
Jihoon Kim <96639809+JH1201@users.noreply.github.com>
2026-09-25 20:05:05 +0900

    [level 2] Title: 호텔 대실, Time: 7.71 ms, Memory: 89.2 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -2,36 +2,33 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] book_time) {
+        
         int answer = 0;
         
-        int[] room = new int[1451];
-        
-        //PriorityQueue<Node> pQ = new PriorityQueue<>();
+        int[] times = new int[1450];
         
         for(int i=0; i<book_time.length; i++) {
-            int start_time = getMin(book_time[i][0]);
-            int end_time = getMin(book_time[i][1]) + 10;
+            int startTime = getMin(book_time[i][0]);
+            int endTime = getMin(book_time[i][1]) + 10;
             
-            for(int j=start_time; j<end_time; j++) {
-                room[j]++;
+            for(int j=startTime; j<endTime; j++) {
+                times[j] += 1;
             }
         }
         
-        for(int i=0; i<=1450; i++) {
-            answer = Math.max(answer, room[i]);
+        for(int i=0; i<times.length; i++) {
+            answer = Math.max(answer, times[i]);
         }
-        
         
         return answer;
     }
     
     public int getMin(String str) {
-        String[] tmp = str.split(":");
+        String[] t = str.split(":");
         
-        int h = Integer.valueOf(tmp[0]) * 60;
-        int m = Integer.valueOf(tmp[1]);
+        int hours = Integer.valueOf(t[0]) * 60;
+        int min = Integer.valueOf(t[1]);
         
-        return h+m;
+        return hours + min;
     }
-    
 }
